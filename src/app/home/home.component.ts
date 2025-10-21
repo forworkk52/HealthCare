8a742f9451f643492f912ed10640ce4cc4e2b648
AsifJamadar352 <asifjamadar352@gmail.com>
2025-10-21 23:35:17 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,33 +6,9 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  isMenuOpen = false;
-
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  closeMenu() {
-    this.isMenuOpen = false;
-  }
 
   submitForm(event: Event) {
     event.preventDefault();
     alert('धन्यवाद! हम शीघ्र संपर्क करेंगे।');
-  }
-
-  // Close menu when clicking outside
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: Event) {
-    const target = event.target as HTMLElement;
-    if (!target.closest('.navbar') && this.isMenuOpen) {
-      this.closeMenu();
-    }
-  }
-
-  // Close menu on escape key press
-  @HostListener('document:keydown.escape')
-  onEscapePress() {
-    this.closeMenu();
   }
 }
