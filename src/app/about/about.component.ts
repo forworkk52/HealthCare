bfb2beec97e4a14ad1af407e5a251d69115783de
AsifJamadar352 <asifjamadar352@gmail.com>
2025-10-23 09:30:28 +0530

    Changes 3

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html'
+})
+export class AboutComponent {
+  // No need for scroll functionality here since it's in the main app component
+}
